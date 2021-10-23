190a4572c6f4e5b768243bc64d791b9ca4df86cf
ashlord666 <ashlord666@gmail.com>
2021-10-24 03:15:28 +0800

    Added v2 which does streaming websocket checks

--- get_supercharger_status_v2.py
+++ get_supercharger_status_v2.py
@@ -8,6 +8,8 @@ from datetime import datetime
 from twython import Twython
 from tendo import singleton
 from logging.handlers import TimedRotatingFileHandler
+import websocket
+import _thread
 
 # region init
 me = singleton.SingleInstance()
@@ -34,10 +36,46 @@ logger.addHandler(trfh)
 logger.addHandler(ch)
 # endregion init
 
+ACCESS_TOKEN = ""
+VEHICLE_ID = ""
+STREAMING = False
+
+def on_message(ws, message):
+    logger.debug(message)
+    
+    # If we get any update message, it is a sign that car is still active
+    if "data:update" in message.decode():
+        global STREAMING
+        STREAMING = True
+
+def on_error(ws, error):
+    logger.debug(error)
+
+def on_close(ws):
+    logger.debug("### closed ###")
+
+def on_open(ws):
+    def run(*args):
+        connect_msg = {
+            "msg_type":"data:subscribe_oauth",
+            "token":ACCESS_TOKEN,
+            "value":"speed,odometer,soc,elevation,est_heading,est_lat,est_lng,power,shift_state,range,est_range,heading",
+            "tag":VEHICLE_ID
+        }
+        logger.debug(f"Sending {json.dumps(connect_msg).encode()}")
+        ws.send(json.dumps(connect_msg).encode())
+        logger.debug("Waiting for 20 secs for update messages")
+        time.sleep(20)
+        ws.close()
+        logger.debug("thread terminating...")
+    _thread.start_new_thread(run, ())
+
+
+
 def main():
     TESLA_CLIENT_ID = ""
     TESLA_CLIENT_SECRET = ""
-    ACCESS_TOKEN = ""
+    global ACCESS_TOKEN
     TWITTER_CONSUMER_KEY = ""
     TWITTER_CONSUMER_SECRET = ""
     TWITTER_ACCESS_TOKEN = ""
@@ -75,6 +113,8 @@ def main():
         logger.debug(vehicles_ret.text)
         vehicles_json = vehicles_ret.json()
         vehicle_0_id = vehicles_json['response'][0]['id']
+        global VEHICLE_ID
+        VEHICLE_ID = f"{vehicles_json['response'][0]['vehicle_id']}"
         vehicle_0_state = vehicles_json['response'][0]['state']
 
         # If car is asleep, we need to wake it
@@ -121,26 +161,40 @@ def main():
                     return
                 counter = counter + 1
 
-        # Car should be awake at this point, so just go ahead and query for the nearby charging sites
-        charging_sites_url = f"https://owner-api.teslamotors.com/api/1/vehicles/{vehicle_0_id}/nearby_charging_sites"
-        charging_sites_ret = my_session.get(charging_sites_url)
-        if charging_sites_ret.status_code != 200:
-            logger.error(f"Unexpected status code {charging_sites_ret.status_code} for {charging_sites_url}")
-            return
-        logger.debug(charging_sites_ret.text)
-        charging_sites_json = charging_sites_ret.json()
-        superchargers_json = charging_sites_json['response']['superchargers']
-        logger.debug(json.dumps(charging_sites_json, indent=4, sort_keys=True))
-        logger.info("\n\n*Singapore SC Slots Free*")
-        tweet_str = "Singapore SC Slots Free #TLKPSC\n"
-        for supercharger in superchargers_json:
-            logger.info(f"{supercharger['name']} : {supercharger['available_stalls']}/{supercharger['total_stalls']}")
-            tweet_str += f"{supercharger['name']} : {supercharger['available_stalls']}/{supercharger['total_stalls']}\n"
-        logger.info(f"Updated at {datetime.now().strftime('%d %b %Y %H:%M:%S')}")
-        tweet_str += f"\nUpdated at {datetime.now().strftime('%d %b %Y %H:%M:%S')}"
-        logger.info("\n")
+        # Additional check so we don't wake up the car if it is trying to sleep.
+        # If streaming websocket gives any update data, that means car is still active (driving/sentry or it just performed some action via API)
+        # Need to conduct more tests to see how soon car will try to sleep after calling nearby_charging_sites
+        websocket.enableTrace(True)
+        ws = websocket.WebSocketApp("wss://streaming.vn.teslamotors.com/streaming/", on_close = on_close, on_error = on_error, on_message = on_message, on_open = on_open)
+        ws.run_forever()
 
-        logger.debug(tweet_str)
+        if STREAMING:
+            logger.debug("Returned from websocket and car is still awake.")
+
+            # Car should be awake at this point, so just go ahead and query for the nearby charging sites
+            charging_sites_url = f"https://owner-api.teslamotors.com/api/1/vehicles/{vehicle_0_id}/nearby_charging_sites"
+            charging_sites_ret = my_session.get(charging_sites_url)
+            if charging_sites_ret.status_code != 200:
+                logger.error(f"Unexpected status code {charging_sites_ret.status_code} for {charging_sites_url}")
+                return
+            logger.debug(charging_sites_ret.text)
+            charging_sites_json = charging_sites_ret.json()
+            superchargers_json = charging_sites_json['response']['superchargers']
+            logger.debug(json.dumps(charging_sites_json, indent=4, sort_keys=True))
+            logger.info("\n\n*Singapore SC Slots Free*")
+            tweet_str = "Singapore SC Slots Free #TLKPSC\n"
+            for supercharger in superchargers_json:
+                logger.info(f"{supercharger['name']} : {supercharger['available_stalls']}/{supercharger['total_stalls']}")
+                tweet_str += f"{supercharger['name']} : {supercharger['available_stalls']}/{supercharger['total_stalls']}\n"
+            logger.info(f"Updated at {datetime.now().strftime('%d %b %Y %H:%M:%S')}")
+            tweet_str += f"\nUpdated at {datetime.now().strftime('%d %b %Y %H:%M:%S')}"
+            logger.info("\n")
+
+            logger.debug(tweet_str)
+
+        else:
+            logger.debug("Returned from websocket and car is trying to sleep. Don't disturb it.")
+
     except:
         e = sys.exc_info()
         logger.error(e)
